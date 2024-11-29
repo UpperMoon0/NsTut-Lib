669ccdcff4be5090a304e18ef7829746ac79a352
UpperMoon0 <60876500+UpperMoon0@users.noreply.github.com>
2024-11-29 17:51:50 +0700

    Init

---
+++ src/main/java/com/nstut/nstutlib/models/StructureBlock.java
@@ -0,0 +1,13 @@
+package com.nstut.nstutlib.models;
+
+import lombok.Data;
+import net.minecraft.world.level.block.Block;
+
+import java.util.Map;
+
+@Data
+public class StructureBlock {
+
+    private Block block;
+    Map<String, String> states;
+}
