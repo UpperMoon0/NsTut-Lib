b9831c71d000b75ed29844b74507a966891ddc57
Vo Nguyen Minh Nhat <60876500+UpperMoon0@users.noreply.github.com>
2026-09-03 02:40:05 +0700

    Bind vanilla item components in 26.1 transaction tests

--- neoforge-26.1.2/src/test/java/com/nstut/nstutlib/recipes/ModRecipeTransactionTest.java
+++ neoforge-26.1.2/src/test/java/com/nstut/nstutlib/recipes/ModRecipeTransactionTest.java
@@ -1,6 +1,8 @@
 package com.nstut.nstutlib.recipes;
 
 import net.minecraft.SharedConstants;
+import net.minecraft.data.registries.VanillaRegistries;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.Identifier;
 import net.minecraft.server.Bootstrap;
 import net.minecraft.world.item.ItemStack;
@@ -23,6 +25,8 @@ class ModRecipeTransactionTest {
     static void bootstrapMinecraftRegistries() {
         SharedConstants.tryDetectVersion();
         Bootstrap.bootStrap();
+        BuiltInRegistries.DATA_COMPONENT_INITIALIZERS.build(VanillaRegistries.createLookup())
+                .forEach(pendingComponents -> pendingComponents.apply());
     }
 
     @Test
