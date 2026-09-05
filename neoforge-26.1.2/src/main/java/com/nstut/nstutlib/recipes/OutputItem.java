5ba1c941e7c0d52b9019ccdd0400e8513229fcc3
Vo Nguyen Minh Nhat <60876500+UpperMoon0@users.noreply.github.com>
2026-09-05 16:51:34 +0700

    Accept 26.1.2 item templates in recipe outputs

--- neoforge-26.1.2/src/main/java/com/nstut/nstutlib/recipes/OutputItem.java
+++ neoforge-26.1.2/src/main/java/com/nstut/nstutlib/recipes/OutputItem.java
@@ -3,6 +3,7 @@ package com.nstut.nstutlib.recipes;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.ItemStackTemplate;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
@@ -11,6 +12,11 @@ public class OutputItem extends RecipeItem {
 
     public OutputItem(ItemStack itemStack, float chance) {
         super(itemStack);
+        this.chance = chance;
+    }
+
+    public OutputItem(ItemStackTemplate itemStackTemplate, float chance) {
+        super(itemStackTemplate);
         this.chance = chance;
     }
 }
